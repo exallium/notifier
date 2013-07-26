cf5f153350cbd0a7653a49e0c03eefb89c03df63
Alex Hart <alex.hart@teamspace.ca>
2013-07-26 16:35:00 -0300

    Move to awesome builder stuff

--- src/ca/exallium/notifier/MainActivity.java
+++ src/ca/exallium/notifier/MainActivity.java
@@ -3,6 +3,9 @@ package ca.exallium.notifier;
 import android.app.Activity;
 import android.os.Bundle;
 import ca.exallium.notifier.gntp.*;
+import ca.exallium.notifier.gntp.builders.NotifyBuilder;
+import ca.exallium.notifier.gntp.builders.RegisterBuilder;
+import ca.exallium.notifier.gntp.models.NotificationType;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -20,24 +23,17 @@ public class MainActivity extends Activity {
 
                 String ip = "172.16.10.164";
 
+
                 try {
-                    Request request = new Request(RequestType.Notify, EncryptionType.None, HashAlgorithm.SHA256, "notasecret");
-                    request.addHeader("Application-Name", "Test");
-                    request.addHeader("Notifications-Count", "1");
-                    request.addHeader("Notification-Name", "Notify 1");
-                    request.sendTo(1, ip, Constants.GNTP_PORT);
+                    RegisterBuilder regBuilder = new RegisterBuilder("notasecret");
+                    NotifyBuilder notify = new NotifyBuilder("notasecret");
 
-                    request = new Request(RequestType.Notify, EncryptionType.None, HashAlgorithm.SHA256, "notasecret");
-                    request.addHeader("Application-Name", "Test");
-                    request.addHeader("Notification-Name", "Notify 1");
-                    request.addHeader("Notification-Title", "Hello");
-                    request.sendTo(1, ip, Constants.GNTP_PORT);
+                    NotificationType.Builder builder = new NotificationType.Builder();
+                    NotificationType type = builder.withName("Notify 1").withDisplayName("Title").build();
 
-                    request = new Request(RequestType.Notify, EncryptionType.None, HashAlgorithm.SHA256, "notasecret");
-                    request.addHeader("Application-Name", "Test");
-                    request.addHeader("Notification-Name", "Notify 1");
-                    request.addHeader("Notification-Title", "Hello");
-                    request.sendTo(1, ip, Constants.GNTP_PORT);
+                    regBuilder.forApp("Test").withCount(1).withHost(ip).withNote(type).send();
+                    notify.forApp("Test").withNote(type).withTitle("Title").withText("Text").withHost(ip).send();
+                    notify.forApp("Test").withNote(type).withTitle("Title").withText("Text 2").withHost(ip).send();
                 } catch (GntpException e) {
                     e.printStackTrace();
                 } catch (UnknownHostException e) {
